1b859428e389b31e57d5faf909335eb78cc2cc23
Ondrej Sika <ondrej@ondrejsika.com>
2019-04-24 19:32:11 +0200

    multistage-image - fix Go app

--- multistage-image/app.go
+++ multistage-image/app.go
@@ -5,14 +5,14 @@ import "fmt"
 import "net/http"
 
 func index(w http.ResponseWriter, r *http.Request) {
-    hostname, _ := os.Hostname()
-    fmt.Fprintf(w, "Hello World from Go! ")
-    fmt.Fprintf(w, hostname)
-    fmt.Fprintf(w, "\n")
+	hostname, _ := os.Hostname()
+	fmt.Fprintf(w, "Hello World from Go! ")
+	fmt.Fprintf(w, hostname)
+	fmt.Fprintf(w, "\n")
 }
 
 func main() {
-    http.HandleFunc("/", index)
-    fmt.Println("Server startded.")
-    http.ListenAndServe(":80", nil)
+	http.HandleFunc("/", index)
+	fmt.Println("Server started.")
+	http.ListenAndServe(":80", nil)
 }
