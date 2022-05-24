ac74101c25eb86b210a4b0d8141da982e90d66ad
Ondrej Sika <ondrej@ondrejsika.com>
2022-05-24 18:15:48 +0200

    cleanup(examples/multistage-image): cleanup go source

--- examples/multistage-image/app.go
+++ examples/multistage-image/app.go
@@ -1,14 +1,14 @@
 package main
 
-import "os"
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func index(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello World from Go! ")
-	fmt.Fprintf(w, hostname)
-	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "Hello World from Go! %s\n", hostname)
 }
 
 func main() {
