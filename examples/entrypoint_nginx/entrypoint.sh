47bdb2f6679dbb027cbaa41dbc581d4d419f3c17
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-31 16:16:46 +0100

    feat(examples/entrypoint_nginx): Make the variable MESSAGE required

--- examples/entrypoint_nginx/entrypoint.sh
+++ examples/entrypoint_nginx/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -z "$MESSAGE" ]; then
+		# The - option suppresses leading tabs but *not* spaces. :)
+		cat >&2 <<-'EOE'
+			Error: Environment variable MESSAGE is required.
+		EOE
+		exit 1
+	fi
+
 echo "<h1>$MESSAGE</h1>" > /usr/share/nginx/html/index.html
 
 exec "$@"
