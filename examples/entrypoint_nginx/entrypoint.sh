f19a4c85bb575cdd0852e81caf08c1e34072c6af
Ondrej Sika <ondrej@ondrejsika.com>
2023-04-10 20:51:34 +0200

    format(examples/entrypoint_nginx): Refactor to match editorconfig settings

--- examples/entrypoint_nginx/entrypoint.sh
+++ examples/entrypoint_nginx/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [ -z "$MESSAGE" ]; then
-		# The - option suppresses leading tabs but *not* spaces. :)
-		cat >&2 <<-'EOE'
-			Error: Environment variable MESSAGE is required.
-		EOE
-		exit 1
-	fi
+# The - option suppresses leading tabs but *not* spaces. :)
+cat >&2 <<-EOF
+Error: Environment variable MESSAGE is required.
+EOF
+exit 1
+fi
 
 echo "<h1>$MESSAGE</h1>" > /usr/share/nginx/html/index.html
 
