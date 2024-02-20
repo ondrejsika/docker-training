354288ab933af9e804b29e50d874674bba3f41a1
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-20 17:56:49 +0100

    fix(examples/multiple-composes): Add app.py back (it was removed by accident)

---
+++ examples/multiple-composes/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(os.environ.get('REDIS', "127.0.0.1"))
+hostname = os.environ['HOSTNAME']
+
+@app.route('/')
+def index():
+    counter = redis.incr('counter')
+    return '<center><h1>%s! %d</h1></center>\n' % (hostname, counter)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='80')
