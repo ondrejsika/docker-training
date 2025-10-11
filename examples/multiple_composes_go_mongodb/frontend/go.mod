c55f7a133090443a34e08ba7247381232b4d0967
Ondrej Sika <ondrej@ondrejsika.com>
2025-10-11 10:53:09 +0200

    feat(examples/multiple_composes_go_mongodb): Add frontend in Go

---
+++ examples/multiple_composes_go_mongodb/frontend/go.mod
@@ -0,0 +1,5 @@
+module frontend
+
+go 1.24.2
+
+require github.com/ondrejsika/counter-frontend-go v0.4.0
---
+++ examples/multiple_composes_go_mongodb/frontend/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ondrejsika/counter-frontend-go/pkg/server"
+)
+
+func main() {
+	server.Server()
+}
