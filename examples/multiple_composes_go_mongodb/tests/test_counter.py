d43d2709c3c04773c4b938afe315ba52c4dc3ec2
Ondrej Sika <ondrej@ondrejsika.com>
2025-12-02 23:50:44 +0100

    feat(examples/multiple_composes_go_mongodb): Add more endpoints to tests

--- examples/multiple_composes_go_mongodb/tests/test_counter.py
+++ examples/multiple_composes_go_mongodb/tests/test_counter.py
@@ -11,7 +11,23 @@ class TestBackend(unittest.TestCase):
         response = requests.get('http://backend:8000/api/counter')
         self.assertEqual(200, response.status_code)
 
+    def test_readyz_response_code(self):
+        response = requests.get('http://backend:8000/readyz')
+        self.assertEqual(200, response.status_code)
+        response = requests.get('http://backend:8000/api/readyz')
+        self.assertEqual(200, response.status_code)
+
+    def test_livez_response_code(self):
+        response = requests.get('http://backend:8000/livez')
+        self.assertEqual(200, response.status_code)
+        response = requests.get('http://backend:8000/api/livez')
+        self.assertEqual(200, response.status_code)
+
 class TestFrontend(unittest.TestCase):
     def test_response_code(self):
         response = requests.get('http://frontend:3000')
+        self.assertEqual(200, response.status_code)
+
+    def test_livez_response_code(self):
+        response = requests.get('http://frontend:3000/api/livez')
         self.assertEqual(200, response.status_code)
