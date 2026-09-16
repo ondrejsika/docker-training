f11a9f86860a25ad5b04d0dd2fcca6e758f73849
Ondrej Sika <ondrej@ondrejsika.com>
2026-09-16 17:34:14 +0200

    feat(examples/compose_backend_tests): Add example compose of backend and tests

---
+++ examples/compose_backend_tests/tests/test_counter.py
@@ -0,0 +1,20 @@
+import unittest
+import requests
+
+
+class TestBackend(unittest.TestCase):
+    def test_api_response_code(self):
+        response = requests.get('http://backend:8000/api/counter')
+        self.assertEqual(200, response.status_code)
+
+    def test_readyz_response_code(self):
+        response = requests.get('http://backend:8000/readyz')
+        self.assertEqual(200, response.status_code)
+        response = requests.get('http://backend:8000/api/readyz')
+        self.assertEqual(200, response.status_code)
+
+    def test_livez_response_code(self):
+        response = requests.get('http://backend:8000/livez')
+        self.assertEqual(200, response.status_code)
+        response = requests.get('http://backend:8000/api/livez')
+        self.assertEqual(200, response.status_code)
