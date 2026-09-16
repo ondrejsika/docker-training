ec0646ac6378bed60cc6dfa105f6d80a130eb6ef
Ondrej Sika <ondrej@ondrejsika.com>
2026-09-16 17:19:52 +0200

    feat(examples/multiple-composes): Add support for env var SUFFIX

--- examples/multiple-composes/app.py
+++ examples/multiple-composes/app.py
@@ -4,12 +4,16 @@ from redis import Redis
 
 app = Flask(__name__)
 redis = Redis(os.environ.get('REDIS', "127.0.0.1"))
+suffix = os.environ.get('SUFFIX', "")
 hostname = os.environ['HOSTNAME']
 
 @app.route('/')
 def index():
     counter = redis.incr('counter')
-    return '<center><h1>%s! %d</h1></center>\n' % (hostname, counter)
+    text = hostname
+    if suffix:
+        text += " " + suffix
+    return '<center><h1>%s! %d</h1></center>\n' % (text, counter)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='80')
